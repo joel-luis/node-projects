ad5c21955796a4c896f8a90c1542c1336f973370
Joel <joelluis73@gmail.com>
2024-03-19 15:50:25 -0300

    model getProducts

--- mongodb/src/controllers/ProductController.js
+++ mongodb/src/controllers/ProductController.js
@@ -1,8 +1,9 @@
 const Product = require('../models/Product')
 
 module.exports = class ProductController {
-  static showProducts(req, res) {
-    res.render('products/all')
+  static async showProducts(req, res) {
+    const products = await Product.getProducts()
+    res.render('products/all', { products })
   }
 
   static createProduct(req, res) {
@@ -10,11 +11,9 @@ module.exports = class ProductController {
   }
 
   static createProductPost(req, res) {
-    const name = req.body.name
-    const price = req.body.price
-    const description = req.body.description
+    const { name, image, price, description } = req.body
 
-    const product = new Product(name, price, description)
+    const product = new Product(name, image, price, description)
 
     product.save()
 
--- mongodb/src/models/Product.js
+++ mongodb/src/models/Product.js
@@ -1,8 +1,9 @@
 const conn = require('../infra/connection')
 
 class Product {
-  constructor(name, price, description) {
+  constructor(name, image, price, description) {
     this.name = name
+    this.image = image
     this.price = price
     this.description = description
   }
@@ -10,11 +11,16 @@ class Product {
   save() {
     const product = conn.db().collection('products').insertOne({
       name: this.name,
+      image: this.image,
       price: this.price,
       description: this.description,
     })
 
     return product
+  }
+
+  static getProducts() {
+    return conn.db().collection('products').find().toArray()
   }
 }
 
